1120c501b1da3e7911da0890020621727a5098b9
Magzz485 <magdaj29379@gmail.com>
2024-11-22 23:40:21 +0100

    Update zadanie9.py

---
+++ zadanie9.py
@@ -0,0 +1,4 @@
+for i in range(1, 101):  # Przechodzimy przez liczby od 1 do 100
+    if i % 7 == 0:  # Sprawdzamy, czy liczba jest podzielna przez 7
+        print(f"Pierwsza liczba podzielna przez 7 to: {i}")
+        break  # Kończymy działanie pętli po znalezieniu pierwszej liczby
