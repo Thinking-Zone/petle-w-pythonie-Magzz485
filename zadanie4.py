fb418106941e674b57f2d70de1a4fe6ebb16dad6
Magzz485 <magdaj29379@gmail.com>
2024-11-22 23:34:58 +0100

    Update zadanie4.py

--- zadanie4.py
+++ zadanie4.py
@@ -2,3 +2,7 @@ tekst = input("Podaj ciąg znaków: ")
 odwrocony = ""
 
 #Tu rozwiązanie :)
+tekst = input("Podaj ciąg znaków: ")
+odwrocony = tekst[::-1]
+
+print("Odwrócony ciąg znaków:", odwrocony)
